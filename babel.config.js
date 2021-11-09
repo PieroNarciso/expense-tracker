7589fec1c1b11ef7478a9c43ac145b6dc87ad6dd
Piero Narciso <pieronarciso988@gmail.com>
2021-11-09 17:44:02 -0500

    Update template

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,22 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
+
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            '@/api': './api',
+            '@/components': './components',
+            '@/screens': './screens',
+            '@/store': './store',
+            '@/interfaces': './interfaces',
+            '@/types': './types'
+          },
+        },
+      ],
+    ],
   };
 };
